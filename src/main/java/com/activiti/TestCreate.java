71d929b3131355f79cdad53005231e38e428bd63
Activiti01 <xvmanjie_beral@163.com>
2021-07-30 17:13:10 +0800

    提交全部 各种配置文件

--- src/main/java/com/activiti/TestCreate.java
+++ src/main/java/com/activiti/TestCreate.java
@@ -1,13 +1,19 @@
 package com.activiti;
 
 import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.engine.ProcessEngines;
 import org.junit.Test;
 
 public class TestCreate {
     @Test
     public void testCreateTable(){
-        ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
+        //1、默认创建方式
+       ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
         System.out.println(processEngine);
+        //2、使用自定义方式---配置文件名字可以自定义--bean 的名字也可以自定义
+   //     ProcessEngineConfiguration processEngineConfigurationFromResource = ProcessEngineConfiguration.createProcessEngineConfigurationFromResource("activiti.cfg.xml");
+     //   ProcessEngine processEngine = processEngineConfigurationFromResource.buildProcessEngine();
+     //   System.out.println(processEngine);
     }
 }
